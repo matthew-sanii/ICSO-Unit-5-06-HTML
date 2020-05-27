1a5a9a5ce100cc0f66bfd9c75014b101089342fb
matthew.sanii <matthew.sanii@mths.ca>
2020-05-27 13:25:18 +0000

    edited index.html and click.js

--- click.js
+++ click.js
@@ -1,19 +1,28 @@
 // JavaScript File
 let response=document.getElementById("response");
+let age;
+let button;
+button=document.getElementById("button");
+age=Math.floor((Math.random() * 40) + 1);
+let answer;
+answer=document.getElementById("answer");
 document.getElementById("button").onclick=function(){Click()};
-function Click(){
-  let answer=document.getElementById("answer");
-  let answerValue;
-  answerValue=String(answer.value);
-  let answer2=document.getElementById("answer2");
-  answer2=Number(answer2.value);
-  if (answerValue == "sunday" || answerValue == "Sunday" || answerValue == "saturday" || answerValue == "Saturday"){
-    response.innerHTML="Time to relax for the weekend!"; // This innerHTML line returns that its the weekend.
+    function Click(){
+    answer=document.getElementById("answer");
+    let answerValue;
+    answerValue=Number(answer.value);
+    if (answerValue == age){
+      response.innerHTML="You Win!"; // This innerHTML line returns that its the weekend.
+    }
+    else if (answerValue < age){
+      response.innerHTML="Too low, try again";
+      button.innerHTML="Try Again";
   }
-  else if (answer2<18 && (answerValue != "saturday" || answerValue != "sunday" || answerValue != "Sunday" || answerValue != "Saturday")){
-    response.innerHTML="Time to go to school!"; // This innerHTML line returns that it's time for school.
-}
-  else if (answer2>=18 && (answerValue != "saturday" || answerValue != "sunday" || answerValue != "Sunday" || answerValue != "Saturday")){
-    response.innerHTML="Time to go to work!"; // This innerHTML line returns that it's time for work.
-}
+    else if (answerValue > age){
+      response.innerHTML="Too high, try again";
+      button.innerHTML="Try Again";
+      }
+    else{
+      response.innerHTML="invalid guess, try again";
+    }
 }
